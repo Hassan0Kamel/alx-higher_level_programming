d99c6e5fb12f5d4bf168809b87ea46e927252a24
Hassan0Kamel <hassan.oka101@gmail.com>
2023-05-09 12:58:55 +0000

    hello

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,17 +13,17 @@ listint_t *insert_node(listint_t **head, int number)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	new->b = number;
+	new->n = number;
 
-	if (node == NULL || node->b >= number)
+	if (node == NULL || node->n >= number)
 	{
-		new->nexto = node;
+		new->next = node;
 		*head = new;
 		return (new);
 	}
-while (node && node->nexto && node->nexto->b < number)
-	node = node->nexto;
-	new->nexto = node->nexto;
-	node->nexto = new;
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+		new->next = node->next;
+		node->next = new;
 		return (new);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -4,18 +4,18 @@
 #include <stdlib.h>
 /**
  * struct listint_s - singly ld list
- * @b: integer
+ * @n: integer
  * @next: points to the ne
  * Description: siked list node structure
  *
  */
 typedef struct listint_s
 {
-	int b;
-	struct listint_s *nexto;
+	int n;
+	struct listint_s *nextn;
 } listint_t;
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int b);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 #endif /* LISTS_H */
