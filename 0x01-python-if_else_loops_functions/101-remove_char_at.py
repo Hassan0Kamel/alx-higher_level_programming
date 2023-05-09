0a027babde1a02e717349ef2ddcda6e3acd41f18
Hassan0Kamel <hassan.oka101@gmail.com>
2023-05-09 14:36:30 +0000

    hello

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+
+def remove_char_at(str, n):
+    """Create a copy of a string without char at position F."""
+    if f < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
