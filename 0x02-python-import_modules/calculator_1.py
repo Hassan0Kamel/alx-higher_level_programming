f72fe60f61e0b84330e88f3b254661e077d14381
Hassan0Kamel <hassan.oka101@gmail.com>
2023-05-11 14:10:25 -0700

    lol

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a + b
+    A
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+    A
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+    Args:
+    A
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
