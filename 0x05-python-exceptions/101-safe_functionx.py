25678a13e2b80324e8776f01aeb1cfd79aa79381
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-29 23:05:20 +0300

    Create 101-safe_functionx.py

---
+++ 0x05-python-exceptions/101-safe_functionx.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """Executes ction safely.
+
+    Args:
+        fct: The function to execute.
+        args: Arguments for fct.
